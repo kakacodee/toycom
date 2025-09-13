a5d047430f9e51bf53da632adf300ec9446a9a18
kaka <kakas13.souza@gmail.com>
2025-09-13 19:25:45 -0300

    adicionando os controladores

---
+++ Toycom/Toycom/Controllers/CarrinhoControler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Toycom.Controllers
+{
+    public class CarrinhoControler : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
